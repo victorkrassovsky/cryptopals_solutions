b54fd5d0940cce9ee79cc67336bedaa2311ffba9
Ba1r <60482377+Ba1r@users.noreply.github.com>
2024-09-11 13:16:39 -0700

    finished c1

--- set1/c1.cpp
+++ set1/c1.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string.h>
 
-char* hex_to_64(char* hex_string){
-    return NULL;
-}
+
+// everything is REALLY unsafe
+
+struct byte_array{
+    int* bytes;
+    size_t size;
+};
 
 int hex_lookup(char p){
     if('0' <= p && p <= '9'){
@@ -15,27 +19,53 @@ int hex_lookup(char p){
     return NULL;
 }
 
-int* hex_to_bytes(char* hex_string){
-    int *byte_arr = (int*)malloc(sizeof(int) * (strlen(hex_string)/2));
+byte_array hex_to_bytes(char* hex_string){
+    byte_array byte_arr = {(int*)malloc(sizeof(int) * (strlen(hex_string)/2)), strlen(hex_string)/2} ;
     for (int i = 0; i < strlen(hex_string); i += 2) {
-        byte_arr[i/2] = hex_lookup(hex_string[i])*16 + hex_lookup(hex_string[i+1]); 
+        byte_arr.bytes[i/2] = hex_lookup(hex_string[i])*16 + hex_lookup(hex_string[i+1]); 
     }
     return byte_arr;
 }
 
-char* bytes_to_64(int* byte_array){
-    return NULL;
+char b64_lookup(int k){
+    if(0 <= k && k <= 25){
+        return 'A' + k;
+    }
+    else if(26 <= k && k <= 51){
+        return 'a' + k - 26;
+    }
+    else if(52 <= k && k <= 61){
+        return '0' + k - 52;
+    }
+    else if(k == 62){
+        return '+';
+    }
+    else if(k == 63){
+        return '/';
+    }
+    return '=';
 }
 
+char* bytes_to_64(byte_array arr){
+    char* b64 = (char*) malloc(sizeof(char)*((arr.size*4)/3 + 1));
+    b64[arr.size*4/3] = '\0';
+    for (int i = 0, j = 0; i < arr.size-2; i += 3){
+        int a = arr.bytes[i];
+        int b = arr.bytes[i+1];
+        int c = arr.bytes[i+2];
+        b64[j++] = b64_lookup(a/4);
+        b64[j++] = b64_lookup((a % 4) * 16 + b/16);
+        b64[j++] = b64_lookup((b % 16) * 4 + c/64);
+        b64[j++] = b64_lookup(c % 64);
+    }
+    return b64;
+}
 
+char* hex_to_64(char* hex_string){
+    return bytes_to_64(hex_to_bytes(hex_string));
+}
 
 int main(int args[]){
     char* hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    int* p = hex_to_bytes(hex);
-    for (int i = 0; i < 5; i++){
-        std::cout << p[i] << "\n";
-    }
-    
-
-    
+    std::cout << hex_to_64(hex);
 }
