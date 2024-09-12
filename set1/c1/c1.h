4f92cec7fed2c5a2c0929541e61bac881074907c
Victor Krassovsky <victorkrassovsky@hotmail.com>
2024-09-11 20:26:24 -0700

    fixed thing

---
+++ set1/c1/c1.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+struct byte_array;
+
+byte_array hex_to_bytes(char*);
+
+char* bytes_to_64(byte_array);
+
+char* hex_to_64(char*);
---
+++ set1/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "c1/c1.h"
+
+int main(){
+  std::cout << hex_to_64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+}
