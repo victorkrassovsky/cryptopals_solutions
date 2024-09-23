147d3745cbe5ed204b680bb882f7606196fbeacb
Ba1r <60482377+Ba1r@users.noreply.github.com>
2024-09-22 19:04:42 -0700

    finished c3

--- set1/set1.py
+++ set1/set1.py
@@ -17,40 +17,44 @@ def xor_hex_strings(h1, h2):
 
 #c3
 
-letterFrequency = {'E' : 12.0,
-'T' : 9.10,
-'A' : 8.12,
-'O' : 7.68,
-'I' : 7.31,
-'N' : 6.95,
-'S' : 6.28,
-'R' : 6.02,
-'H' : 5.92,
-'D' : 4.32,
-'L' : 3.98,
-'U' : 2.88,
-'C' : 2.71,
-'M' : 2.61,
-'F' : 2.30,
-'Y' : 2.11,
-'W' : 2.09,
-'G' : 2.03,
-'P' : 1.82,
-'B' : 1.49,
-'V' : 1.11,
-'K' : 0.69,
-'X' : 0.17,
-'Q' : 0.11,
-'J' : 0.10,
-'Z' : 0.07 }
+letterFrequency = {'e' : 12.0,
+'t' : 9.10,
+'a' : 8.12,
+'o' : 7.68,
+'i' : 7.31,
+'n' : 6.95,
+'s' : 6.28,
+'r' : 6.02,
+'h' : 5.92,
+'d' : 4.32,
+'l' : 3.98,
+'u' : 2.88,
+'c' : 2.71,
+'m' : 2.61,
+'f' : 2.30,
+'y' : 2.11,
+'w' : 2.09,
+'g' : 2.03,
+'p' : 1.82,
+'b' : 1.49,
+'v' : 1.11,
+'k' : 0.69,
+'x' : 0.17,
+'q' : 0.11,
+'j' : 0.10,
+'z' : 0.07 }
+
+common_digraphs = "th er on an re he in ed nd ha at en es of or nt ea ti to it st io le is ou ar as de rt ve"
+common_digraphs = common_digraphs.split(' ')
 
 def score(s):
     total = 0
     for c in s:
         if(c in letterFrequency.keys()):
             total += letterFrequency[c]
-        else:
-            total -= 2
+    for i in range(0,len(s)-2):
+        if s[i:i+2] in common_digraphs:
+            total += 10
     return total
 
 def single_byte_xor(hex_string):
@@ -63,6 +67,6 @@ def single_byte_xor(hex_string):
             and best_score < score(decrypted_bytes.decode('utf-8'))):
             best_score = score(decrypted_bytes.decode('utf-8'))
             best_i = i
-    return (best_i, b''.join([(best_i^x).to_bytes(1,'big') for x in b]).decode('utf-8'))
+    return (best_i, b''.join([(best_i^x).to_bytes(1,'big') for x in b]))
             
 #c4
