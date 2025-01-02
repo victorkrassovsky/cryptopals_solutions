cfa4ad6b73824a468301f56581da7c3ce78bd42f
Ba1r <60482377+Ba1r@users.noreply.github.com>
2025-01-01 22:21:13 -0800

    finished c17

--- set3/set3.py
+++ set3/set3.py
@@ -8,7 +8,7 @@ def ciphertext_generator():
     key = b'{\xf2ieB\xd7v)^\x98m|F\x89\\\x82'
     with open('c17.txt', 'r') as f:
         lines = [b64.b64decode(l.strip()) for l in f]
-    pt = lines[random.randint(0,len(lines))]
+    pt = lines[random.randint(0,len(lines)-1)]
     ct = cbc.cbc_encrypt(pt, key)
     return ct
 
@@ -18,7 +18,7 @@ def ciphertext_generator():
 def padding_oracle(byte_file):
     key = b'{\xf2ieB\xd7v)^\x98m|F\x89\\\x82'
     try:
-        cbc.cbc_decrypt(byte_file, key)
+        pt = cbc.cbc_decrypt(byte_file, key)
     except Exception as inst:
         (c,) = inst.args
         if c == 'Invalid padding':
@@ -37,15 +37,15 @@ def xor_strings(a,b):
     else:
         return b''.join([(x^y).to_bytes(1,'big') for x,y in zip(a[:len(b)], b)])
 
-# decrypts a ciphertext given by ciphertext_generator with access to padding_oracle        
+# decrypts a ciphertext given by ciphertext_generator with access to padding_oracle       
 def c17():
     # get ciphertext
     ct = ciphertext_generator()
     ct_blocks = [ct[i:i+16] for i in range(0, len(ct), 16)]
     result = b''
     # recover each block
+    # TAKES A FEW SECONDS
     for i in range(1, len(ct_blocks)):
-        print(i)
         # we need to modify the i-1th ct block to recover ith block
         prev_block = ct_blocks[i-1]
         cur_block = ct_blocks[i]
@@ -54,9 +54,10 @@ def c17():
         for j in range(0, 256):
             temp_block = xor_strings(prev_block, j.to_bytes(16,'big'))
             corrupt_block = xor_strings(temp_block, bytes(14) + b'\x01\x00')
-            if padding_oracle(temp_block + cur_block) and (not padding_oracle(corrupt_block + cur_block)):
+            if padding_oracle(temp_block + cur_block) and padding_oracle(corrupt_block + cur_block):
                 # now j xor pt has last byte \x01, hence last byte of pt is \x01 xor j
                 pt[-1] = j ^ 1
+                #print(pt[-1])
                 break
 
         # recover the kth byte
@@ -65,10 +66,8 @@ def c17():
                 temp_block = xor_strings(prev_block, bytes(16-k) + j.to_bytes(1,'big') +  bytes(pt[-(k-1):]))
                 temp_block = xor_strings(temp_block, bytes(17-k)  + (k-1)*(k.to_bytes(1,'big')))
                 if padding_oracle(temp_block + cur_block):
-                    pt[-k] = j
+                    pt[-k] = j ^ k
                     break
-        print(pt)
-        result += pt
+        #print(pt)
+        result += bytes(pt)
     return result
-
-#TODO: Fix whatever is broken
